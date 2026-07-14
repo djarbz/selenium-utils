a64cd42a37147998252263b1e99728e79451cd3c
default <austin@arbour.me>
2026-07-14 12:44:24 -0500

    feat(ci): Deploy dependabot

--- .coder/extensions.sh
+++ .coder/extensions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-code-server --install-extension charliermarsh.ruff
-code-server --install-extension ms-python.python
-# code-server --install-extension ms-python.vscode-pylance # Not available in 3rd party VSCode
-code-server --install-extension tamasfe.even-better-toml
-code-server --install-extension github.vscode-github-actions
+# code-server --install-extension charliermarsh.ruff
+# code-server --install-extension ms-python.python
+# # code-server --install-extension ms-python.vscode-pylance # Not available in 3rd party VSCode
+# code-server --install-extension tamasfe.even-better-toml
+# code-server --install-extension github.vscode-github-actions
